a0b70df743aa18f423b3d170e352eac8bea6c734
d.kubitskiy <denis.kubicky@gmail.com>
2020-10-15 17:53:00 +0600

    информация о маркерах удаление маркеров

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -1,5 +1,5 @@
 import { ThrowStmt } from '@angular/compiler';
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import { MapService } from '../services/map.service'
 
 @Component({
@@ -8,7 +8,10 @@ import { MapService } from '../services/map.service'
   styleUrls: ['./left-panel.component.scss']
 })
 export class LeftPanelComponent implements OnInit {
-
+  @ViewChild('deskInput', {static: true}) deskInput : ElementRef
+  @ViewChild('formContainer', {static: true}) formContainer : ElementRef
+  @ViewChild('lngInput', {static: true}) lngInput : ElementRef
+  @ViewChild('latInput', {static: true}) latInput : ElementRef
   coordinates = {
     lat: '',
     lng: '',
@@ -21,14 +24,38 @@ export class LeftPanelComponent implements OnInit {
 
   ngOnInit() {
     console.log('init conmonent')
+    this.mapService.deskInput = this.deskInput
+    this.mapService.formContainer = this.formContainer
+    this.mapService.latInput = this.latInput
+    this.mapService.lngInput = this.lngInput
 
   }
 
- async drawPoint() {
+setDescription(e) {
+  this.mapService.currentCoordinates.description = e.target.value
+}
 
+
+ async drawPoint() {
+  this.clearInputs()
+  // this.deskInput.nativeElement.value = ''
+console.log(this.deskInput)
 
  this.mapService.drawPoint()
 
+  }
+
+  deleteLayer() {
+    this.clearInputs()
+    // this.mapService.coordinateShow = true
+    this.formContainer.nativeElement.hidden = true
+    this.mapService.deleteLayer()
+  }
+
+  clearInputs() {
+    this.deskInput.nativeElement.value = ''
+  this.lngInput.nativeElement.value =  ''
+  this.latInput.nativeElement.value = ''
   }
 
 }
