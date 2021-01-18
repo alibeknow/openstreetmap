a00659b54647632b980253a96189f2e5deccac34
dKante <denis.kubicky@gmail.com>
2021-01-18 17:17:36 +0600

    debug build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:4000',
-  tileUrl: 'http://localhost:4002/mapboxLight/{z}/{x}/{y}.png'
+  tileUrl: 'http://localhost:4002/mapboxLight/{z}/{x}/{y}.png',
+  graphopperUrl: 'http://localhost:8989'
 };
